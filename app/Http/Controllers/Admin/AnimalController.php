7d00dcf2cd63bcf07d7fd6ce1a21d274b737cba4
martinaliporata <martinaliporata@gmail.com>
2024-07-22 22:24:03 +0200

    implementing controller, web.php, views

---
+++ app/Http/Controllers/Admin/AnimalController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Animal;
+use Illuminate\Http\Request;
+
+class AnimalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $animals = Animal::all();
+        return view('admin.animals.index', compact('animals'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $animals = Animal::findOrFail($id);
+        return view('admin.animals.show', compact('animals'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
