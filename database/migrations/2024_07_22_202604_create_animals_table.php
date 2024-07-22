33137a971d247e14b90473d01c2388ab2b242c25
martinaliporata <martinaliporata@gmail.com>
2024-07-22 22:33:12 +0200

    creating table

---
+++ database/migrations/2024_07_22_202604_create_animals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('species');
+            $table->string('weight');
+            $table->string('alimentation');
+            $table->string('extintion');
+            $table->string('colour');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
