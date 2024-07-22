be8e95b6c62a2d68bb8d6eba79950f659f24b0aa
martinaliporata <martinaliporata@gmail.com>
2024-07-22 22:49:33 +0200

    added seeder with faker

--- database/seeders/AnimalSeeder.php
+++ database/seeders/AnimalSeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
+use App\Models\Animal;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class AnimalSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for($i=0; $i<30; $i++){
+            $animals = new Animal();
+            $animals->name = $faker->word(20);
+            $animals->species = $faker->word(20);
+            $animals->weight = $faker->word(20);
+            $animals->alimentation= $faker->word(20);
+            $animals->extintion = $faker->word(20);
+            $animals->colour = $faker->word(20);
+            $animals->save();
+        }
     }
 }
