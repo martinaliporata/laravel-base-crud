af80b12ca98b9b4f5c64a9a05ff7c7ceab24c89e
martinaliporata <martinaliporata@gmail.com>
2024-07-29 12:08:32 +0200

    fixed old

--- resources/views/admin/animals/edit.blade.php
+++ resources/views/admin/animals/edit.blade.php
@@ -28,23 +28,24 @@
                 @method("PUT")
                 @csrf
                 <label for="name">Name</label>
-                <input class="form-control form-control-sm" type="text" placeholder="Animal name" aria-label="Animal name" id="name" name="name" value="{{$animal->name}}">
+                {{-- old di name c'è? sì. allora dammi pokemon name --}}
+                <input class="form-control form-control-sm" type="text" placeholder="Animal name" aria-label="Animal name" id="name" name="name" value="{{old ('name', $animal->name)}}">
                 <label for="image">Image_url</label>
-                <input class="form-control form-control-sm" type="text" placeholder="Animal image" aria-label="image" id="image" name="image" value="{{$animal->image_url}}">
+                <input class="form-control form-control-sm" type="text" placeholder="Animal image" aria-label="image" id="image" name="image" value="{{old ('image_url', $animal->image_url)}}">
                 <label for="species">Species</label>
-                <input class="form-control form-control-sm" type="text" placeholder="species" aria-label="species" id="species" name="species" value="{{$animal->species}}">
+                <input class="form-control form-control-sm" type="text" placeholder="species" aria-label="species" id="species" name="species" value="{{old ('species', $animal->species)}}">
                 <label for="weight">Weight</label>
-                <input class="form-control form-control-sm" type="number" placeholder="weight" aria-label="weight" id="weight" name="weight" value="{{$animal->weight}}">
+                <input class="form-control form-control-sm" type="number" placeholder="weight" aria-label="weight" id="weight" name="weight" value="{{old ('weight', $animal->weight)}}">
                 <label for="alimentation">Alimentation</label>
-                <input class="form-control form-control-sm" type="text" placeholder="alimentation" aria-label="alimentation" id="alimentation" name="alimentation" value="{{$animal->alimentaton}}">
+                <input class="form-control form-control-sm" type="text" placeholder="alimentation" aria-label="alimentation" id="alimentation" name="alimentation" value="{{old ('alimentation', $animal->alimentation)}}">
                 <label for="extintion">Extintion</label>
-                <input class="form-control form-control-sm" type="text" placeholder="extintion" aria-label="extintion" id="extintion" name="extintion" value="{{$animal->extintion}}">
+                <input class="form-control form-control-sm" type="text" placeholder="extintion" aria-label="extintion" id="extintion" name="extintion" value="{{old ('extintion', $animal->extintion)}}">
                 <label for="colour">Colour</label>
-                <input class="form-control form-control-sm" type="text" placeholder="colour" aria-label="colour" id="colour" name="colour" value="{{$animal->colour}}">
+                <input class="form-control form-control-sm" type="text" placeholder="colour" aria-label="colour" id="colour" name="colour" value="{{old ('colour', $animal->colour)}}">
                 <label for="habitat">Habitat</label>
-                <input class="form-control form-control-sm" type="text" placeholder="habitat" aria-label="habitat" id="habitat" name="habitat" value="{{$animal->habitat}}">
+                <input class="form-control form-control-sm" type="text" placeholder="habitat" aria-label="habitat" id="habitat" name="habitat" value="{{old ('habitat', $animal->habitat)}}">
                 <label for="life duration">Life duration</label>
-                <input class="form-control form-control-sm" type="text" placeholder="life duration" aria-label="life duration" id="life duration" name="life duration" value="{{$animal->life_duration}}">
+                <input class="form-control form-control-sm" type="text" placeholder="life duration" aria-label="life duration" id="life duration" name="life duration" value="{{old ('life_duration', $animal->life_duration)}}">
                 <div class="d-flex justify-content-between">
                     <input type="submit" value="Update {{$animal->name}}" class="btn btn-primary">
                     <input type="reset" value="Reset fields" class="btn btn-warning">
